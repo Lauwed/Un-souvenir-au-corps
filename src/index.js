e84865951e18cb3bde1380caafd0cac6c265de2b
lauweddu <ldurieux97@gmail.com>
2020-05-15 23:22:29 +0200

    first files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import CycleWheel from './components/CycleWheel';
+import SkyBackground from './components/SkyBackground';
+// import style
+import "./assets/sass/app.scss";
+
+let skyBackground = new SkyBackground(new CycleWheel()).init();
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
